7c6945c3fbf6a176c3f9113e5bc6550ce2c278f7
Harry Grenier <hgrenier@uri.edu>
2025-02-26 12:57:10 -0500

    i think its done 7

--- Labs/Lab5-ajax/js/book-html.js
+++ Labs/Lab5-ajax/js/book-html.js
@@ -1,50 +1,52 @@
-//SET OPACITY TO 0.5
-
-
+// Function to dim all images to opacity 0.5
 function dimImages() {
-   let images = document.querySelectorAll("img");
-   images.forEach(img => {
-       img.style.opacity = "0.5";
-   });
-}
-
-
-// Call the function to apply the effect
-dimImages();
-
-
-//LOAD HTML FILE
-
-
-function loadHTML(filePath) {
-   // Create an XMLHttpRequest object
-   let xhr = new XMLHttpRequest();
-   images.forEach(img => {
-    img.addEventListener("click", function() {
-        images.forEach(i => i.style.opacity = "0.5"); // Reset all images
-        this.style.opacity = "1"; // Set opacity of the clicked image
-        });
+    let images = document.querySelectorAll("img");
+    images.forEach(img => {
+        img.style.opacity = "0.5"; // Set all images to dimmed
     });
-
-
-   // Define what happens when the response is loaded
-   xhr.onreadystatechange = function() {
-       if (xhr.readyState === 4) { // Request is complete
-           if (xhr.status === 200) { // Success
-               document.getElementById("details").innerHTML = xhr.responseText;
-           let images = document.querySelectorAll("img");
-
-           } else {
-               console.error("Error loading file:", xhr.status, xhr.statusText);
-           }
-       }
-   };
-
-
-   // Prepare the GET request
-   xhr.open("GET", filePath, true);
-
-
-   // Send the request
-   xhr.send();
-}
+ }
+ 
+ 
+ // Call dimImages() on page load
+ window.onload = function() {
+    dimImages();
+ };
+ 
+ 
+ // Function to load the HTML file and highlight the clicked image
+ function loadHTML(filePath, clickedImg) {
+    // Create an XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
+ 
+ 
+    console.log(clickedImg);
+ 
+ 
+    // Define what happens when the response is loaded
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) { // Request is complete
+            if (xhr.status === 200) { // Success
+                document.getElementById("details").innerHTML = xhr.responseText;
+ 
+ 
+                // Reset opacity for all images
+                dimImages();
+ 
+ 
+                // Set clicked image's opacity to 1
+                clickedImg.style.opacity = "1";
+            } else {
+                console.error("Error loading file:", xhr.status, xhr.statusText);
+            }
+        }
+    };
+ 
+ 
+    // Prepare the GET request
+    xhr.open("GET", filePath, true);
+ 
+ 
+    // Send the request
+    xhr.send();
+ }
+ 

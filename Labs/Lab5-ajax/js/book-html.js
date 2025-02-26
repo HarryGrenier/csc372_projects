56b6635f8171870c1a23149ea357e18a1b4bcbe1
Harry Grenier <hgrenier@uri.edu>
2025-02-26 13:29:55 -0500

    i think its done 9

--- Labs/Lab5-ajax/js/book-html.js
+++ Labs/Lab5-ajax/js/book-html.js
@@ -1,42 +1,77 @@
 // Function to dim all images to opacity 0.5
-function dimAllImages() {
+function dimImages() {
     let images = document.querySelectorAll("img");
     images.forEach(img => {
-        img.style.opacity = "0.5";
+        img.style.opacity = "0.5"; // Set all images to dimmed
     });
-}
-
-function loadBookDetails(filePath, clickedImg) {
+ }
+ 
+ 
+ // Call dimImages() on page load
+ window.onload = function() {
+    dimImages();
+ };
+ 
+ 
+ document.addEventListener("DOMContentLoaded", function() {
+    let bookImage = document.getElementById("don-quixote-img");
+ 
+ 
+    if (bookImage) {
+        bookImage.addEventListener("click", function() {
+            loadHTML("data/dickens-data.html", this);
+        });
+    }
+ });
+ 
+ 
+ document.addEventListener("DOMContentLoaded", function() {
+    let bookImage = document.getElementById("two-cities-img");
+ 
+ 
+    if (bookImage) {
+        bookImage.addEventListener("click", function() {
+            loadHTML("data/tolkien-data.html", this);
+        });
+    }
+ });
+ 
+ 
+ document.addEventListener("DOMContentLoaded", function() {
+    let bookImage = document.getElementById("lotr-img");
+ 
+ 
+    if (bookImage) {
+        bookImage.addEventListener("click", function() {
+            loadHTML("data/cervantes-data.html", this);
+        });
+    }
+ });
+ 
+ 
+ function loadHTML(filePath, clickedImg) {
     let xhr = new XMLHttpRequest();
-
+ 
+ 
     xhr.onload = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            document.getElementById("details").innerHTML = xhr.responseText;
-            dimAllImages();
-            clickedImg.style.opacity = "1";
-        } else if (xhr.status !== 200) {
-            console.error("Error loading file:", xhr.status, xhr.statusText);
+        if (xhr.readyState === 4) { // Request is complete
+            if (xhr.status === 200) { // Success
+                document.getElementById("details").innerHTML = xhr.responseText;
+ 
+ 
+                // Reset opacity for all images
+                dimImages();
+ 
+ 
+                clickedImg.style.opacity = "1";
+            } else {
+                console.error("Error loading file:", xhr.status, xhr.statusText);
+            }
         }
     };
-
+ 
+ 
     xhr.open("GET", filePath, true);
     xhr.send();
-}
-
-// Assign event listeners to book images
-document.getElementById('don-quixote-img').addEventListener('click', function() {
-    loadBookDetails('../data/cervantes-data.html', this);
-});
-
-document.getElementById('two-cities-img').addEventListener('click', function() {
-    loadBookDetails('../data/dickens-data.html', this);
-});
-
-document.getElementById('lotr-img').addEventListener('click', function() {
-    loadBookDetails('../data/tolkien-data.html', this);
-});
-
-// Call dimAllImages() on page load
-window.onload = function() {
-    dimAllImages();
-};
+ }
+ 

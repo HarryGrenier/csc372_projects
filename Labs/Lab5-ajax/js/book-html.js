c1fcfc3f4cafa628bb97da578810c3510cc5bbde
Harry Grenier <hgrenier@uri.edu>
2025-02-26 12:50:47 -0500

    i think its done 4

--- Labs/Lab5-ajax/js/book-html.js
+++ Labs/Lab5-ajax/js/book-html.js
@@ -26,7 +26,13 @@ function loadHTML(filePath) {
        if (xhr.readyState === 4) { // Request is complete
            if (xhr.status === 200) { // Success
                document.getElementById("details").innerHTML = xhr.responseText;
-               this.img.style.opacity = "1";
+           let images = document.querySelectorAll("img");
+           images.forEach(img => {
+               img.addEventListener("click", function() {
+                   images.forEach(i => i.style.opacity = "0.5"); // Reset all images
+                   this.style.opacity = "1"; // Set opacity of the clicked image
+               });
+           });
            } else {
                console.error("Error loading file:", xhr.status, xhr.statusText);
            }
